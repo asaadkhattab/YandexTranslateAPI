dd29456d7341b720d086315cd048fc966a3f7cd6
asaadkhattab <asaad@khattab.live>
2020-05-10 19:30:03 -0400

    Added user input

--- script.py
+++ script.py
@@ -4,12 +4,16 @@ API_KEY = 'trnsl.1.1.20200510T212315Z.6e3b997c11482427.15d523e425bd9106df55174cb
 
 url = 'https://translate.yandex.net/api/v1.5/tr.json/translate'
 
+print('Type an English word or sentence to convert it to Arabic')
 
 
-params = dict(key=API_KEY, text='Good', lang='en-ar')
+params = dict(key=API_KEY, text=input(), lang='en-ar')
 
 res = requests.get(url, params=params)
 
 print(res.text)
 
 print(res.status_code)
+
+json = res.json()
+print(json['text'][0])
